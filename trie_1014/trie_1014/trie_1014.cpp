37a291923d05c91fea3cdf79f81a8b61c64ef5b0
Tim Chan <tim147@outlook.com>
2017-07-01 11:51:29 +0800

    trie

---
+++ trie_1014/trie_1014/trie_1014.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class trie {
+	bool node;
+	trie *next[26];
+};
+
+void create_trie(string new_word) {
+
+}
+
+void delete_trie(trie* head) {
+
+}
+
+int main() {
+
+}
